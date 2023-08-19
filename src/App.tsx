b180ba411e00f835ad44192695324ab64ee89beb
Muhammed Oguz <muhammed.oguz@ekos.ai>
2023-08-19 16:40:36 +0300

    Implement with openAI api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,59 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { OpenAI } from "openai";
 import "./App.css";
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPEN_AI_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 function App() {
   const [input, setInput] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
 
-  const handleAddTodo = () => {
+  const handleOpenAIReq = useCallback(async (value: string) => {
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "Generate emoji from given text",
+          },
+          {
+            role: "assistant",
+            content: "😊",
+          },
+          {
+            role: "user",
+            content: value,
+          },
+        ],
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      console.log(response);
+      const emoji = response.choices[0].message.content;
+      return emoji;
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const handleAddTodo = async () => {
     console.log("input", input);
     // send input to openAI
     // get response from openAI
     // const emojiData = response.data.emojiData;
     // const newInput = emojiData + input;
 
-    setTodos([...todos, input]);
+    const emoji = await handleOpenAIReq(input);
+    const newInput = emoji + input;
+
+    setTodos([...todos, newInput]);
     setInput("");
   };
 
