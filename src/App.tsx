dd97d518c8caa95ac2e440dca8e3e995307f6af8
ayseglaydgan <aysegul.99@windowslive.com>
2023-08-19 16:15:30 +0300

    handleAddTodo function added

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
+
+  const handleAddTodo = () => {
+    console.log("input", input);
+    // send input to openAI
+    // get response from openAI
+    // const emojiData = response.data.emojiData;
+    // const newInput = emojiData + input;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
 
   return (
     <div>
       <h1>TODO APP</h1>
-      <form
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        onSubmit={(e: any) => {
-          e.preventDefault();
-
-          const newTodo = e.target[0].value;
-          setTodos((prev) => [...prev, newTodo]);
-        }}
-      >
-        <input type="text" name="todo" />
-        <button type="submit">Add</button>
-      </form>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>{todo}</li>
